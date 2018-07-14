cc66e9cc4d02f4b3d69b3e943ec5692fa6a738fe
Laurence Hughes <laurence@fuzzylogicdigital.com>
2018-07-14 10:10:55 +0200

    No longer ignoring the perch config file from version control, for two reasons: 1) The Perch setup routine requires an empty config.php file; and 2) we can handle both local and production environment variables in the same config file/s which negates the need to ignore the file because the environments are different.

---
+++ dist/console/config/config.php
@@ -0,0 +1,22 @@
+<?php
+    define('PERCH_LICENSE_KEY', 'P31807-TSK957-BPX059-ADC661-SNL011');
+
+    define("PERCH_DB_USERNAME", 'root');
+    define("PERCH_DB_PASSWORD", 'docker');
+    define("PERCH_DB_SERVER", "db");
+    define("PERCH_DB_DATABASE", "db_fuzzylogic");
+    define("PERCH_DB_PREFIX", "perch3_");
+    
+    define('PERCH_TZ', 'UTC');
+
+    define('PERCH_EMAIL_FROM', 'laurence@fuzzylogicdigital.com');
+    define('PERCH_EMAIL_FROM_NAME', 'Laurence Hughes');
+
+    define('PERCH_LOGINPATH', '/console');
+    define('PERCH_PATH', str_replace(DIRECTORY_SEPARATOR.'config', '', __DIR__));
+    define('PERCH_CORE', PERCH_PATH.DIRECTORY_SEPARATOR.'core');
+
+    define('PERCH_RESFILEPATH', PERCH_PATH . DIRECTORY_SEPARATOR . 'resources');
+    define('PERCH_RESPATH', PERCH_LOGINPATH . '/resources');
+    
+    define('PERCH_HTML5', true);
