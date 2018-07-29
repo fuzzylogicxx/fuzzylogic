d25b93a1d1eae6d22412877913636b514d0a484e
Laurence Hughes <laurence@fuzzylogicdigital.com>
2018-07-29 16:59:41 +0100

    A version which uses FS Emeric (don't think I like it...)

--- dist/inc/html_head.php
+++ dist/inc/html_head.php
@@ -25,15 +25,42 @@
 	<link rel="stylesheet" href="/css/normalize.css">
 	<link rel="stylesheet" href="/css/main.css">
 
-	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro&amp;subset=latin-ext" rel="stylesheet"> 
+	<!-- <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro&amp;subset=latin-ext" rel="stylesheet">  -->
+	<!-- “Source Serif, Open Sans, Lato, Clear Sans, Merriweather, Fira, Overpass and Alegreya” -->
+	<!-- <link href="https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i&amp;subset=latin-ext" rel="stylesheet"> -->
+	<!-- <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i&amp;subset=latin-ext" rel="stylesheet"> -->
+	<!-- <link href="https://fonts.googleapis.com/css?family=Fira+Sans:400,400i,700,700i" rel="stylesheet"> -->
+<!-- 	<link href="https://fonts.googleapis.com/css?family=Alegreya:400,400i,700,700i&amp;subset=latin-ext" rel="stylesheet"> -->
+
 
 	<style>
+		@font-face {
+			font-family: 'FSEmeric';
+			src: url('/fonts/fsemericwebregular/FSEmericWeb-Regular.eot'); /* IE9 */
+			src: url('/fonts/fsemericwebregular/FSEmericWeb-Regular.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */ 
+			url('/fonts/fsemericwebregular/FSEmericWeb-Regular.woff') format('woff'); /* Modern Browsers */
+			font-weight: normal;
+			font-style: normal;
+		}
+		@font-face {
+			font-family: 'FSEmeric';
+			src: url('/fonts/fsemericwebbook/FSEmericWeb-Book.eot'); /* IE9 */
+			src: url('/fonts/fsemericwebbook/FSEmericWeb-Book.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */ 
+			url('/fonts/fsemericwebbook/FSEmericWeb-Book.woff') format('woff'); /* Modern Browsers */
+			font-weight: 300;
+			font-style: normal;
+		}
 		body {
-			font-family: 'Source Sans Pro', sans-serif;
+			font-family: 'FSEmeric', sans-serif; /* 'Fira Sans', *//* 'Open Sans',*/ /*  'Lato',  */ /* 'Source Sans Pro', */
 			margin: 0;
 			padding: 0;
 			line-height: 1.5;
+			font-size: 17px;
+			font-weight: 300;
+			/*color: #fff;*/
+			/*background-color: rgb(38, 38, 38);*/
 			color: #333;
+			-webkit-font-smoothing: antialiased;
 		}
 		body {
 		    font-feature-settings: "liga" 1;
@@ -52,6 +79,14 @@
 		        font-variant-numeric: oldstyle-nums;
 		    }
 		}
+		a {
+			color: #4db7ac;
+			/*color: green;*/ /*rgb(158, 216, 210);*/ /* color: rgb(185, 225, 220); */
+			/*color: rgb(255,255,255);*/
+		}
+		a:hover {
+
+		}
 		header, .container {
 			margin: 0 20px;
 			max-width: 38em;
@@ -61,6 +96,17 @@
 				margin-left: auto;
 				margin-right: auto;
 			}
+		}
+		abbr, code.markup.element, code.markup.attribute {
+    		text-decoration: none;
+    		font-family: inherit;
+    		font-variant: all-small-caps;
+		}
+		.hfeed, 
+		.hfeed li {
+			list-style: none;
+			padding-left: 0;
+			margin-left: 0;
 		}
 	</style>
 
