d6c002c6e9c03faad50adb96d75214af7973777d
Laurence Hughes <laurence@fuzzylogicdigital.com>
2018-07-29 20:53:29 +0100

    Hiding blog comment form and other comments-related stuff for the moment

--- dist/blog/post.php
+++ dist/blog/post.php
@@ -17,9 +17,9 @@
         </div>
     </div>
 
-	<?php perch_blog_post_comments(perch_get('s')); ?>
+	<?php //perch_blog_post_comments(perch_get('s')); ?>
 
-	<?php perch_blog_post_comment_form(perch_get('s')); ?>
+	<?php //perch_blog_post_comment_form(perch_get('s')); ?>
 
 </div>
 
