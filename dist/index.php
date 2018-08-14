20008975b067716e36a6b7eebfe948c5c80609a4
Laurence Hughes <laurence@fuzzylogicdigital.com>
2018-08-14 14:37:53 +0100

    The homepage benefits from a top-level heading (even a daft one)

--- dist/index.php
+++ dist/index.php
@@ -5,7 +5,7 @@
 
 <div class="intro">
     <h1>YO</h1>
-    <p>Hi &ndash; I’m Laurence Hughes, a web developer and director at <a href="http://www.greenhilldigital.com/">Greenhill</a>, music maker in <a href="http://nuclearfami.ly/">The Nuclear Family</a>, player of records and pinger of pongs.</p>
+    <p>I’m Laurence Hughes, a web developer and director at <a href="http://www.greenhilldigital.com/">Greenhill</a>, music maker in <a href="http://nuclearfami.ly/">The Nuclear Family</a>, player of records and pinger of pongs.</p>
 
     <p><a href="https://fuzzylogic.me/" rel="me">Fuzzy Logic</a> is my online home, where I test-drive web stuff, log discoveries and share my thoughts on code, music and more.</p>
 </div>
---
+++ dist/testcontent/index.php
@@ -0,0 +1,332 @@
+<?php $page = ['title' => 'Homepage']; ?>
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/page_start.php'); ?>
+
+<main>
+
+<article>
+
+<h1>Give me 26 lead soldiers and I will conquer the world.</h1>
+<p class="article-meta">
+    <i><time class="pubdate updated" datetime="2018-06-14T09:01:00-04:00" itemprop="datePublished">June 14, 2018</time></i>
+    <!-- <span class="byline-additional meta">in <a href="/">blogging</a>, <a href="/">side-projects</a></span> -->
+</p>
+
+<!-- <p class="standfirst">Every developer should have a blog. Here’s why, and how to stick with it.</p>
+
+<img 
+    srcset="/img/320x240.jpg 320w,
+            /img/480x360.jpg 480w,
+            /img/640x480.jpg 640w, 
+            /img/752x564.jpg 752w, 
+            /img/960x720.jpg 960w" 
+    sizes="(max-width: 59em) 90vw, 
+           (max-width: 105em) 50vw,
+           960px" 
+    src="/img/960x720.jpg" alt="A referee uses the we.ref app during a football match at Broadwood Stadium near Glasgow"> -->
+
+<div class="article-body">
+    
+<p>A blog is useful for many reasons. It can be<span style="color: green;">c</span>ome <a href="/">a source of leads</a>, it can <span style="color: red;">b</span>e the place where, in the future, you might sell your products if you want to become an indie developer, or it can simply be the place where you have your audience and express your ideas.</p>
+
+<p>I’ve been blogging for more than 11 years now, more or less consistently — although sometimes I stopped for too long. I recently revamped the blog and started to write consistently — very consistently — to the point I now write every single day of the week. I’ve already seen a lot of good results.</p>
+
+<!-- <figure>
+    <picture> -->
+        <!-- <source srcset="https://s3-eu-west-1.amazonaws.com/clearleft-v5/uploads/_1080x1080_crop_center-center_85/bhcc-index.jpg?mtime=20171101102133" media="(min-width: 57.5em)">
+        <source srcset="https://s3-eu-west-1.amazonaws.com/clearleft-v5/uploads/_800x800_crop_center-center_85/bhcc-index.jpg?mtime=20171101102133" media="(min-width: 42.5em)"> -->
+<!--         <img src="/img/4to3.jpg" alt="" style="width: 100%; height: 100%; object-fit: cover">
+    </picture>
+    <figcaption>Noel Gallagher's High Flying Birds</figcaption>
+</figure> -->
+
+<table>
+    <thead>
+        <tr>
+            <th>Col 1</th>
+            <th>Col 2</th>
+            <th>Col 3</th>
+        </tr>
+    </thead>
+    <tfoot>
+        <tr>
+            <th>Col 1</th>
+            <th>Col 2</th>
+            <th>Col 3</th>
+        </tr>
+    </tfoot>
+    <tbody>
+        <tr>
+            <td>Val 1</td>
+            <td>Val 2</td>
+            <td>Val 3</td>
+        </tr>
+        <tr>
+            <td>Val 1</td>
+            <td>Val 2</td>
+            <td>Val 3</td>
+        </tr>
+    </tbody>
+</table>
+
+<p>Sed eu sapien sed metus dapibus imperdiet sit amet non diam. Vivamus quis dolor eros. Donec ut libero ut nisi imperdiet hendrerit vitae at lacus. Nunc convallis molestie tortor eget ultricies. Pellentesque imperdiet accumsan tellus, id varius metus scelerisque at. Donec fermentum sapien id libero pulvinar, at ornare magna ultrices. In hac habitasse platea dictumst. Donec tincidunt nibh id massa dignissim consectetur. Nulla a tortor vehicula, imperdiet velit vel, elementum quam. Aliquam id leo quis odio molestie sodales. Sed eget luctus enim. Suspendisse id tristique risus. Nunc vitae eros erat.</p>
+
+<p><small>Small print text here</small></p>
+
+<dl>
+    <dt>Item One Definition Title</dt>
+    <dd>Item One Definition Data</dd>
+    <dt>Item Two Definition Title</dt>
+    <dd>Item Two Definition Data</dd>
+</dl>
+
+<h2>Give me 26 lead soldiers and I will conquer the world.</h2>
+
+<h3>Give me 26 lead soldiers and I will conquer the world.</h3>
+
+<p>Anyone has a unique angle, a perspective on something that’s worth sharing. You might think you don’t know as much as person X, but person Y might have much less experience than you and would benefit from reading your thoughts and learnings.</p>
+
+<p>Also, the best moment to teach something is right after you’ve learned it, because you remember how not knowing about it feels.</p>
+
+<h3>"I'm not a good writer"</h3>
+
+<p>I’m not either, but I don’t care.</p>
+
+<p>Just remember: you will never become a good writer unless you practice writing every day for years. You <em>will become</em> a good writer eventually.</p>
+
+<blockquote>
+    <p>One more attribute the modern typographer must have: the capacity for taking great pains with seemingly unimportant detail. To him, one typographical point must be as important as one inch, and he must harden his heart against the accusation of being too fussy.</p>
+    <footer>— Hans P. Schmoller, in ‘Book Design Today’, <span class="cite-and-pubdate"><cite>Printing Review</cite>, 1951</span></footer>
+</blockquote>
+
+<p>Text following a blockquoute and preceding a pull quote.</p>
+
+<blockquote class="pullquote" cite="http://inl-agency.com/">
+    <p>Always available, always happy to find good solutions, clear thinking. <br />Greenhill GET IT.</p>
+    <footer>– Rich Thomas, Director INL Agency, Geneva</footer>
+</blockquote>
+
+<h3>A third level heading that will run ontf two lines because it has a lot of crap in it</h3>
+
+<p>Some text here.</p>
+
+<h4>Give me 26 lead soldiers and I will conquer the world.</h4>
+
+<p>Some text here.</p>
+
+<ul>
+    <li>First list item</li>
+    <li>Second list item</li>
+    <li>Third list item
+        <ul>
+            <li>inner item one</li>
+            <li>inner item two</li>
+            <li>inner item three</li>
+        </ul>
+    </li>
+</ul>
+
+<ol>
+    <li>First list item</li>
+    <li>Second list item</li>
+</ol>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dignissim dolor et laoreet consequat. Sed eu augue arcu. Maecenas lectus tortor, pretium at ex id, tempor tristique tellus. Pellentesque efficitur tincidunt quam et ornare. Donec dictum, sapien sit amet pulvinar sagittis, urna erat fringilla nisl, eget iaculis felis ipsum iaculis metus. Fusce egestas finibus sem sit amet laoreet. Donec in lacus nulla. Donec iaculis libero et egestas tempor.</p>
+
+<p>Sed eu sapien sed metus dapibus imperdiet sit amet non diam. Vivamus quis dolor eros. Donec ut libero ut nisi imperdiet hendrerit vitae at lacus. Nunc convallis molestie tortor eget ultricies. Pellentesque imperdiet accumsan tellus, id varius metus scelerisque at. Donec fermentum sapien id libero pulvinar, at ornare magna ultrices. In hac habitasse platea dictumst. Donec tincidunt nibh id massa dignissim consectetur. Nulla a tortor vehicula, imperdiet velit vel, elementum quam. Aliquam id leo quis odio molestie sodales. Sed eget luctus enim. Suspendisse id tristique risus. Nunc vitae eros erat.</p>
+
+</div> <!-- .article-body -->
+
+</article>
+
+<form action="">
+    <label for="input-1" class="form-group">
+        <b class="label-text">Input 1</b>
+        <input type="text" id="input-1" />
+    </label>
+    <div class="error-msg">This is required.</div>
+    <label for="input-2" class="form-group">
+        <b class="label-text">Input 2</b>
+        <input type="url" id="input-2" />
+    </label>
+    <label class="form-group" for="input-2">
+        <b class="label-text">Birthday</b>
+        <div class="custom-select">
+            <select id="input-3">
+                <option selected disabled>Month</option>
+                <option>January</option>
+                <option>February</option>
+                <option>March</option>
+                <option>April</option>
+                <option>May</option>
+                <option>June</option>
+                <option>July</option>
+                <option>August</option>
+                <option>September</option>
+                <option>October</option>
+                <option>November</option>
+                <option>December</option>
+            </select>
+        </div>
+    </label>
+    <label for="input-4" class="form-group">
+        <b class="label-text">Input 4</b>
+        <input type="text" id="input-4" />
+    </label>
+    <div class="error-msg">This is required.</div>
+    <div class="submit-wrap">
+        <button type="submit">Submit</button>
+    </div>
+</form>
+
+<!-- <section class="teaser">
+    <h2 class="teaser-heading">Selected Services</h2>
+    <ul class="teaser-list">
+        <li>
+            <a class="wrapping-anchor" href="/">
+                <img srcset="/img/320x240.jpg 320w,
+                            /img/480x360.jpg 480w,
+                            /img/640x480.jpg 640w, 
+                            /img/752x564.jpg 752w, 
+                            /img/960x720.jpg 960w" 
+                        sizes="(max-width: 59em) 90vw, 
+                                (max-width: 105em) 50vw,
+                                960px" 
+                        src="/img/960x720.jpg" alt="A referee uses the we.ref app during a football match at Broadwood Stadium near Glasgow">
+                <h3 class="teaser-item-heading">iOS and Android App Development</h3>
+                <p>Two-way communication between your central database and your customer app</p>
+            </a>
+        </li>
+        <li>
+            <a class="wrapping-anchor" href="/">
+                <img src="/img/4to3.jpg" alt="" style="width: 100%; height: 100%; object-fit: cover">
+                <h3 class="teaser-item-heading">Digital Restaurant Menus</h3>
+                <p>Update menus instantly; let customers order complex items and promotions from their table(t)</p>
+            </a>
+        </li>
+        <li>
+            <a class="wrapping-anchor" href="/">
+                <img src="/img/4to3.jpg" alt="" style="width: 100%; height: 100%; object-fit: cover">
+                <h3 class="teaser-item-heading">E-commerce Websites</h3>
+                <p>Two-way communication between your central database and your customer app</p>
+            </a>
+        </li>
+    </ul>
+    <a class="btn btn-more btn-teaser-browse-all" href="/services/">Browse all services</a>
+</section>
+ -->
+<section class="teaser">
+    <h2 class="teaser-heading">Latest Entries</h2>
+    
+<ul class="teaser-list hfeed listing">
+    <li class="hentry">
+        <h3 class="teaser-item-heading"><a href="/journal/react-native-versus-ionic-2" rel="bookmark" class="entry-title">React Native versus Ionic 2</a></h3>
+        <!--  -->
+        <p class="byline">
+            <span class="author-and-date">by <span class="p-author h-card">Samantha Kirkwood</span> <time class="dt-published" datetime="2018-07-19 08:50:00">19 July 2018</time></span> 
+            <!-- <span class="byline-additional">
+                
+            </span> -->
+        </p>
+        <div class="description entry-summary">
+            <p>Evaluating differences, benefits and drawbacks.</p>
+            <!-- <p>Comments: 0</p> -->
+        </div>
+    </li>
+
+    <li class="hentry">
+        <h3 class="teaser-item-heading"><a href="/journal/2018-07-03-ampersand-conference-2018" rel="bookmark" class="entry-title">Ampersand Conference 2018</a></h3>
+        <!-- <img src="/console/resources/img2021-w50h50.jpg" alt="Ampersand Conference 2018" /> -->
+        <p class="byline">
+            <span class="author-and-date">by <span class="p-author h-card">Laurence Hughes</span> <time class="dt-published" datetime="2018-07-03 12:19:00">03 July 2018</time></span> 
+            <!-- <span class="byline-additional">
+                
+            </span> -->
+        </p>
+        <div class="description entry-summary">
+            <p>Nam in dignissim lectus, sed posuere enim.</p>
+            <!-- <p>Comments: 0</p> -->
+        </div>
+    </li>
+
+    <li class="hentry">
+        <h3 class="teaser-item-heading"><a href="/journal/were-going-to-aea-chicago-2018" rel="bookmark" class="entry-title">We’re going to AEA Chicago 2018</a></h3>
+        <!--  -->
+        <p class="byline">
+            <span class="author-and-date">by <span class="p-author h-card">Laurence Hughes</span> <time class="dt-published" datetime="2018-06-25 14:47:00">25 June 2018</time></span> 
+            <!-- <span class="byline-additional">
+                
+                    <a href="/journal/archive.php?cat=blogcat2" class="p-category">blogcat2</a>
+                
+            </span> -->
+        </p>
+        <div class="description entry-summary">
+            <p>dfghdfghd</p>
+            <!-- <p>Comments: 0</p> -->
+        </div>
+    </li>
+
+    <li class="hentry">
+        <h3 class="teaser-item-heading"><a href="/journal/2018-06-21-the-making-of-our-v2-website" rel="bookmark" class="entry-title">The making of our v2 website</a></h3>
+        <!--  -->
+        <p class="byline">
+            <span class="author-and-date">by <span class="p-author h-card">Laurence Hughes</span> <time class="dt-published" datetime="2018-06-21 16:18:00">21 June 2018</time></span> 
+            <!-- <span class="byline-additional">
+                
+                    <a href="/journal/archive.php?cat=thinking" class="p-category">Thinking</a>
+                
+            </span> -->
+        </p>
+        <div class="description entry-summary">
+            <p>Cras tortor elit, sollicitudin vel purus non, tempor porta diam.</p>
+            <!-- <p>Comments: 0</p> -->
+        </div>
+    </li>
+
+    <li class="hentry">
+        <h3 class="teaser-item-heading"><a href="/journal/laravel-live-uk-2018" rel="bookmark" class="entry-title">Laravel Live UK 2018</a></h3>
+        <!--  -->
+        <p class="byline">
+            <span class="author-and-date">by <span class="p-author h-card">Laurence Hughes</span> <time class="dt-published" datetime="2018-06-21 16:13:00">21 June 2018</time></span> 
+            <!-- <span class="byline-additional">
+                
+                    <a href="/journal/archive.php?cat=blogcat2" class="p-category">blogcat2</a>
+                
+            </span> -->
+        </p>
+        <div class="description entry-summary">
+            <p>Laravel is “The PHP Framework For Web Artisans” - here’s Kieran’s appraisal of their June ‘18 London conference</p>
+            <!-- <p>Comments: 0</p> -->
+        </div>
+    </li>
+
+    <li class="hentry">
+        <h3 class="teaser-item-heading"><a href="/journal/2018-06-18-book-review-going-offline" rel="bookmark" class="entry-title">Book Review: "Going Offline"</a></h3>
+        <!--  -->
+        <p class="byline">
+            <span class="author-and-date">by <span class="p-author h-card">Laurence Hughes</span> <time class="dt-published" datetime="2018-06-18 13:55:00">18 June 2018</time></span> 
+            <!-- <span class="byline-additional">
+                
+                    <a href="/journal/archive.php?cat=thinking" class="p-category">Thinking</a>
+                
+                    <a href="/journal/archive.php?cat=blogcat2" class="p-category">blogcat2</a>
+                
+            </span> -->
+        </p>
+        <div class="description entry-summary">
+            <p>Etiam sit amet elementum nisi. Mauris interdum quam velit, et luctus diam malesuada ac.</p>
+            <!-- <p>Comments: 0</p> -->
+        </div>
+    </li>
+
+    </ul>
+        
+    <a class="btn btn-more btn-teaser-browse-all" href="/journal/">Browse all entries</a>
+</section>
+
+</main>
+
+<aside>
+  Sidebar
+</aside>
+
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/page_end.php'); ?>
