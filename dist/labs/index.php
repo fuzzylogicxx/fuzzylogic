f79de6eb515d5cff2b8b1374c11afa4552b87fcf
Laurence Hughes <laurence@fuzzylogicdigital.com>
2018-10-19 10:38:45 +0100

    Added content to my CSS Grid Layout, moved it into a new website section called Labs, added a Labs index

---
+++ dist/labs/index.php
@@ -0,0 +1,20 @@
+<?php $page = ['title' => 'Homepage']; ?>
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/page_start.php'); ?>
+
+<main>
+
+	<h1>Labs</h1>
+	
+	<h2>CSS Grid Experiments</h2>
+	<ul>
+		<li><a href="/labs/grid-layout-1.html">CSS Grid Layout 1</a></li>
+	</ul>
+
+</main>
+
+<aside role="complementary">
+    <h3>Elsewhere online</h3>
+    <p>You’ll find me tweeting on <a rel="me" href="https://twitter.com/fuzzylogicx">Twitter</a>; posting photos on <a rel="me" href="https://www.instagram.com/fuzzylogicx/">Instagram</a>; on Soundcloud sharing <a rel="me" href="https://soundcloud.com/the_nuclear_family">music</a> and <a rel="me" href="https://soundcloud.com/fuzzylogicx">mixes</a>; collecting, buying and selling records on <a rel="me" href="https://www.discogs.com/user/laurence">Discogs</a>; and coding on <a rel="me" href="https://github.com/fuzzylogicxx">Github</a> and <a rel="me" href="https://codepen.io/fuzzylogicx/">CodePen</a>.</p>
+</aside>
+
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/page_end.php'); ?>
