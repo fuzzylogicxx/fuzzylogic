ce5536239d714fc17920604ef21fdcb7d8488ad3
Laurence Hughes <laurence@fuzzylogicdigital.com>
2018-11-04 13:47:17 +0000

    Added the first HTML/CSS version of the site as an item (for posterity) in "Labs"

--- dist/labs/index.php
+++ dist/labs/index.php
@@ -10,6 +10,11 @@
 		<li><a href="/labs/grid-layout-1.html">CSS Grid Layout 1</a></li>
 	</ul>
 
+	<h2>Previous versions of this site</h2>
+	<ul>
+		<li><a href="/labs/site-version-1.html">Version #1</a></li>
+	</ul>
+
 </main>
 
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/sidebar.php'); ?>
