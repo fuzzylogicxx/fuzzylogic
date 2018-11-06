9ef6e5961a89cfe1a1565dd44633e36036e6e639
Laurence Hughes <laurence@fuzzylogicdigital.com>
2018-11-06 15:56:51 +0000

    These are WIP but no reason not to commit them.

---
+++ dist/bookmarks/index.php
@@ -0,0 +1,20 @@
+<?php $page = ['title' => 'Bookmarks']; ?>
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/page_start.php'); ?>
+
+<main>
+
+	<h1>Bookmarks</h1>
+	<p>Coming soon.</p>
+
+	<!-- Bookmark
+		<title> of page which links to item
+		description (although generally short this can be multi-line/multi-paragraph, but generally just quotes/snippets/observations rather than prose-y)
+		tags -->
+		
+
+
+</main>
+
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/sidebar.php'); ?>
+
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/page_end.php'); ?>
