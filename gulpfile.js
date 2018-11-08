36eacf44ea33d0bca2eed9001b3e93940ca99a58
Laurence Hughes <laurence@fuzzylogicdigital.com>
2018-11-08 16:52:07 +0000

    Revved CSS filename

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ gulp.task('sass', function () {
     .pipe(sass({precision: 10}).on('error', sass.logError))
     .pipe(sourcemaps.write())
     .pipe(autoprefixer())
-    .pipe(rename('screen_v3.css'))
+    .pipe(rename('screen_v4.css'))
     .pipe(gulp.dest(output));
 });
 
