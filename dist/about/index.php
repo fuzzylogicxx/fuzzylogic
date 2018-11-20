c66c587916aca9fb1ef766cafb2a2fd94b81fe67
Laurence Hughes <laurence@fuzzylogicdigital.com>
2018-11-20 23:09:07 +0000

    Added some "About" content

--- dist/about/index.php
+++ dist/about/index.php
@@ -4,8 +4,17 @@
 <main>
 
 	<h1>About</h1>
-	<p>Coming soon.</p>
+	
+	<h2>Web</h2>
+	<p>I’ve been working as a web developer since 2003. The web inspired me then (as it still does now) firstly because a single person can publish something that is instantly available around the world; and secondly because of the guiding principle that it is an information-sharing resource <em>accessible to all</em>.</p>
+
+	<p>Shortly after cutting my teeth as a developer I read <a href="https://en.wikipedia.org/wiki/Designing_with_Web_Standards#Summary">Designing with Web Standards</a> and that had a big impact upon me. In the years since then I’ve always enjoyed balancing cutting-edge technology alongside a responsibility to make my work accessible and user-friendly as far as possible.</p>
+
 	<!-- <p>I’m a web designer-developer (specialising in front end development but also accomplished in backend dev)</p> -->
+
+	<h2>Music</h2>
+	<p>I’ve been collecting records since I was fifteen and playing or making music for longer still. Growing up in the eighties, electronic music was the sound that grabbed my attention and its been a big part of my life ever since.</p>
+	<p>My first release (under the name Elijah) was in 2002 – a track called Ice 2 Go on <a href="https://www.discogs.com/Various-Better-By-Design/release/110485"><em>Better by Design</em></a> from Glasgow label Iridite. Nowadays I co-run and release music on a label called <a href="https://www.discogs.com/The-Nuclear-Family-vs-Other-Lands-Surface-Noise/release/12684949">The Nuclear Family</a>.</p>
 
 </main>
 
