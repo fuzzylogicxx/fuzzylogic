f41e0a18e26a2a74cca80c18f722890709c390e4
Laurence Hughes <laurence@fuzzylogicdigital.com>
2018-11-21 22:14:00 +0000

    Show list items within news articles when on the homepage; content tweak on the About page.

--- dist/about/index.php
+++ dist/about/index.php
@@ -6,9 +6,9 @@
 	<h1>About</h1>
 	
 	<h2>Web</h2>
-	<p>I’ve been working as a web developer since 2003. The web inspired me then (as it still does now) firstly because a single person can publish something that is instantly available around the world; and secondly because of the guiding principle that it is an information-sharing resource <em>accessible to all</em>.</p>
+	<p>I’ve been working as a web developer since 2003. The web inspired me then (as it still does now) firstly because a single person can create and publish something that becomes instantly available around the world; and secondly because of the guiding principle that should be an information-sharing resource <em>accessible to all</em>.</p>
 
-	<p>Shortly after cutting my teeth as a developer I read <a href="https://en.wikipedia.org/wiki/Designing_with_Web_Standards#Summary">Designing with Web Standards</a> and that had a big impact upon me. In the years since then I’ve always enjoyed balancing cutting-edge technology alongside a responsibility to make my work accessible and user-friendly as far as possible.</p>
+	<p>Shortly after cutting my teeth as a developer I read <a href="https://en.wikipedia.org/wiki/Designing_with_Web_Standards#Summary">Designing with Web Standards</a> and that had a big impact on me. In the years since then I’ve always enjoyed balancing cutting-edge technology alongside usability and accessibility.</p>
 
 	<!-- <p>I’m a web designer-developer (specialising in front end development but also accomplished in backend dev)</p> -->
 
