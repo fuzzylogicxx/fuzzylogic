5ef46122a481921d9da1877d795e2b013679497b
Laurence Hughes <laurence@fuzzylogicdigital.com>
2018-11-22 00:18:37 +0000

    Another "About" tweak.

--- dist/about/index.php
+++ dist/about/index.php
@@ -6,7 +6,8 @@
 	<h1>About</h1>
 	
 	<h2>Web</h2>
-	<p>I’ve been working as a web developer since 2003. The web inspired me then (as it still does now) firstly because a single person can create and publish something that becomes instantly available around the world; and secondly because of the guiding principle that it should be an information-sharing resource <em>accessible to all</em>.</p>
+	<p>I’ve been working as a web developer since 2003.</p>
+	<p>The web inspired me then (as it still does now) firstly because a single person can create and publish something that becomes instantly available around the world; and secondly because of the guiding principle that it should be an information-sharing resource <em>accessible to all</em>.</p>
 
 	<p>Shortly after cutting my teeth as a developer I read <a href="https://en.wikipedia.org/wiki/Designing_with_Web_Standards#Summary">Designing with Web Standards</a> and that had a big impact on me. In the years since then I’ve always enjoyed balancing cutting-edge technology alongside usability and accessibility.</p>
 
