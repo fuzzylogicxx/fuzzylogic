d2e893d50a39f1d0db47f6e024fcf8b2139daac4
Laurence Hughes <laurence@fuzzylogicdigital.com>
2019-01-03 11:45:44 +0000

    Added robots.txt and a sitemap on the advice of https://varvy.com/

---
+++ dist/sitemap/index.php
@@ -0,0 +1,16 @@
+<?php $page = ['title' => 'Sitemap']; ?>
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/page_start.php'); ?>
+
+<main>
+
+	<h1>Sitemap</h1>
+	
+	<ul>
+		<li><a href="https://fuzzylogic.me">Home</a></li>
+	</ul>
+
+</main>
+
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/sidebar.php'); ?>
+
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/page_end.php'); ?>
