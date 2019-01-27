0cd4f27c0c1024e4adb8d2a4b6829de01ecd8f33
Laurence Hughes <laurence@fuzzylogicdigital.com>
2019-01-27 17:30:20 +0000

    CSS: i) added new style for Codepen embeds; ii) added font-display: swap; to assist web font loading. Revved the stylesheet filename.

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ gulp.task('sass', function () {
     .pipe(sass({precision: 10}).on('error', sass.logError))
     .pipe(sourcemaps.write())
     .pipe(autoprefixer())
-    .pipe(rename('screen_v11.css'))
+    .pipe(rename('screen_v12.css'))
     .pipe(gulp.dest(output));
 });
 
