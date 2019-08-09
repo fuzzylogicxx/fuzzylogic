65c7c315b1f7640ddaafb4008480ad58cd4e25c7
Laurence Hughes <laurence@fuzzylogicdigital.com>
2019-08-09 15:30:55 +0100

    Installed the npm "file-size" module for more human-readable file sizes.

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,10 @@
 const fs = require("fs");
+const filesize = require("file-size");
 module.exports = (function(eleventyConfig) {
  eleventyConfig.addFilter("filesize", function(path) {
   let stat = fs.statSync(path);
   if( stat ) {
-   return (stat.size/1024).toFixed(2) + " KB";
+   return filesize(stat.size).human();
   }
   return "";
  });
