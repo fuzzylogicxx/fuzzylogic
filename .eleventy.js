c068a1a6252a22bcb696649a9515f38aadfc800d
Laurence Hughes <laurence@fuzzylogicdigital.com>
2019-08-12 13:36:21 +0100

    Told 11ty which file types to "pass through" (copy over) without processing

--- .eleventy.js
+++ .eleventy.js
@@ -4,32 +4,39 @@ const filesize = require("file-size");
 
 module.exports = (function(eleventyConfig) {
 
-//
-eleventyConfig.addFilter("filesize", function(path) {
-  let stat = fs.statSync(path);
-  if( stat ) {
-    return filesize(stat.size).human();
+  //
+  eleventyConfig.addFilter("filesize", function(path) {
+    let stat = fs.statSync(path);
+    if( stat ) {
+      return filesize(stat.size).human();
+    }
+    return "";
+  });
+
+  // We’ll use a dedicated .eleventyignore file to tell 11ty what not to process, rather than .gitignore
+  eleventyConfig.setUseGitIgnore(false);
+
+  // Copy (without processing) the following directories and file types to the compiled site folder
+  eleventyConfig.addPassthroughCopy('src/favicon.ico');
+  eleventyConfig.addPassthroughCopy('src/site.webmanifest');
+  eleventyConfig.addPassthroughCopy('src/safari-pinned-tab.svg');
+  eleventyConfig.addPassthroughCopy('src/robots.txt');
+
+
+  // Base Config
+  return {
+    templateFormats: [
+      "html",
+      "liquid",
+      "md",
+      "njk",
+      // Now list any non-11ty-recognized-template-extensions, and files of these types will just be copied directly through to /dist. Feels a bit hacky but documentation advocates it!
+      "png"
+    ],
+    dir: {
+      input: 'src',
+      output: 'dist'
+    }
   }
-  return "";
-});
-
-// We’ll use a dedicated .eleventyignore file to tell 11ty what not to process, rather than .gitignore
-eleventyConfig.setUseGitIgnore(false);
-
-// Copy (without processing) the `assets` directory to the compiled site folder
-eleventyConfig.addPassthroughCopy('src/assets');
-
-// If you use a subdirectory, it’ll copy using the same directory structure.
-//eleventyConfig.addPassthroughCopy("css/fonts");
-
-// Base Config
-return {
-  dir: {
-    input: 'src',
-    output: 'dist'
-  }
-}
-
-
 
 });
