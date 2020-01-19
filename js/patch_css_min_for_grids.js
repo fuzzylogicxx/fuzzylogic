d75256e69c76c54efd226967e428f420c127ef76
Laurence Hughes <laurence@fuzzylogicdigital.com>
2020-01-19 22:06:28 +0000

    Second pass at bookshelf grid

---
+++ js/patch_css_min_for_grids.js
@@ -0,0 +1,41 @@
+;(function () {
+
+/*!
+ * Use ResizeObserver to apply the class .aboveMin to a given grid container (.grid) when its width is greater than it’s "data-min" value.
+ *
+ */
+function observeGrid(gridNode) {
+  // Feature detect ResizeObserver
+  if ('ResizeObserver' in window && !CSS.supports('width', `min(20rem, 100%)`)) {
+    // Get the min value from data-min="[min]"
+    const min = gridNode.dataset.min;
+    // Create a proxy element to measure and convert
+    // the `min` value (which might be em, rem, etc) to `px`
+    const test = document.createElement('div');
+    test.style.width = min;
+    gridNode.appendChild(test);
+    const minToPixels = test.offsetWidth;
+    gridNode.removeChild(test);
+
+    const ro = new ResizeObserver(entries => {
+      for (let entry of entries) {
+        // Get the element's current dimensions
+        const cr = entry.contentRect;
+        // `true` if the container is wider than the minimum
+        const isWide = cr.width > minToPixels;
+        // toggle the class conditionally
+        gridNode.classList.toggle('aboveMin', isWide);
+      }
+    });
+
+    ro.observe(gridNode);
+  }
+}
+
+// patch support for CSS’s min() so that our CSS Grid ".grid" components are intrinsically responsive.
+var grids = document.querySelectorAll('.grid');
+for (var i = 0; i < grids.length; i++) {
+  observeGrid(grids[i]);
+}
+
+})();
