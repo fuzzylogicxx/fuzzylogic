babff6f30e4a9d3189b91f4a9ac29b5453cb6215
Laurence Hughes <laurence@fuzzylogicdigital.com>
2020-11-22 19:50:31 +0000

    Update photos.js

--- _data/photos.js
+++ _data/photos.js
@@ -1,11 +1,11 @@
 const fetch = require("node-fetch");
 require('dotenv').config();
 
-const apiUrl = `https://389853243577723:${process.env.CLOUDINARY_API_SECRET}@api.cloudinary.com/v1_1/fuzzylogic/resources/image?max_results=12`;
+const apiUrl = `https://389853243577723:${process.env.CLOUDINARY_API_SECRET}@api.cloudinary.com/v1_1/fuzzylogic/resources/image?max_results=500;
 
 const getPhotos = async function(url) {
   console.log(url);
-  return fetch(url, { method: 'GET', max_results: 11 })
+  return fetch(url, { method: 'GET' })
     .then(res => res.json())
     .then(json => {
       console.log(json);
