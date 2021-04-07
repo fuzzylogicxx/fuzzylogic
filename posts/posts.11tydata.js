eca69de77726afd9099f70b67c762b4a16c86fbd
Laurence Hughes <laurence@fuzzylogicdigital.com>
2021-04-07 18:38:31 +0100

    Adds a JavaScript 11tydata file for the posts directory, setting all posts (via eleventyComputed) to be excluded from collections if draft=true. Additionally if the environment is production the output file is not created either, to avoid search engines crawling unfinished blog posts. I also moved what was previolusly in posts.json into this js data file, and removed the livePosts collection because I don’t think it’s necessary now that draft posts are excluded from the 'posts' collection by our new, more 11ty-centric excludeFromCollection approach.

--- posts/posts.11tydata.js
+++ posts/posts.11tydata.js
@@ -1,13 +1,23 @@
 module.exports = {
+  tags: [
+    "posts"
+  ],
+  layout: "layouts/post.njk",
+  mainImage: {
+    "aspectRatioWidth": 320,
+    "aspectRatioHeight": 240
+  },
   eleventyComputed: {
     /**
-     * Adds support for drafts.
+     * Custom handling for draft posts so they don’t show up unexpectedly.
      * If a page has `draft: true` in its YAML frontmatter then this snippet
      * will set its permalink to false and exclude it from collections.
      *
      * For dev builds we will always render the page.
      */
     permalink: data => {
+      // in dev environments we want to still access a draft post by visiting its URL directly,
+      // but in production the draft post should not be output to a physical file and available to search engines.
       if (process.env.NODE_ENV === 'production' && data.draft) {
         return false;
       }
@@ -15,7 +25,7 @@ module.exports = {
       return data.permalink;
     },
     eleventyExcludeFromCollections: data => {
-      if (process.env.NODE_ENV === 'production' && data.draft) {
+      if (data.draft) {
         return true;
       }
 
