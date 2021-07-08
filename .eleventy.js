b10f011bfd5e36b4914470a6cdda9e5465667ea4
Laurence Hughes <laurence@fuzzylogicdigital.com>
2021-07-08 19:21:26 +0100

    Whitelists CSS classes which include a colon to avoid purgecss mismatches when the CSS class includes an escaping slash but the HTML attribute doesn’t

--- .eleventy.js
+++ .eleventy.js
@@ -40,6 +40,10 @@ module.exports = function(eleventyConfig) {
       content: [{ raw: content }],
       css: ['_includes/css/main.css'],
       keyframes: true,
+      // safelist (i.e. always include) our ”single-rule utility classes”.
+      // They use a colon which needs escaped in CSS but not in HTML.
+      // This makes it difficult to get purgecss to match the HTML to the CSS, so safelisting them works around that.
+      safelist: [/\:/]
     });
 
     return content.replace(
