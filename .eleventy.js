b2da374354e6e33afec63ed772bc0858c81d8a89
Laurence Hughes <laurencehughes1976@gmail.com>
2024-12-29 21:49:56 +0000

    Remove sections I no longer maintain or want (#157)
    * Removes inspiration page
    
    * Deletes bookshelf
    
    * Deletes records_for_sale page and data
    
    * Deletes photos page and data file which fetched from Cloudinary API
    
    * Deletes forestry CMS folder
    
    * Deletes html_head include because it had no useful content and was unused
    
    * Deletes bookmarker script and lambda folder / JS file which posted to Github via its API
    
    * Removes node-fetch; now that I’ve removed lots of the 'data' files I don’t expect I need node-fetch any longer.
    
    * Deletes everything relating to my old DIY search feature
    
    * Deletes no-longer-needed JS patch for CSS min(), which allows deleting my only JS script too
    
    * Removes a layout that was only for photos (so no longer required)

--- .eleventy.js
+++ .eleventy.js
@@ -137,39 +137,6 @@ module.exports = function(eleventyConfig) {
     }
   });
 
-  // 'squash' filter (used when creating the search index) | credit Phil Hawksworth
-  // Make a search index string by removing duplicated words
-  // and removing less useful, common short words
-  // @param {String} text
-
-  eleventyConfig.addFilter('squash', text => {
-    var content = new String(text);
-
-    // all lower case, please
-    var content = content.toLowerCase();
-
-    // remove all html elements and new lines
-    var re = /(&lt;.*?&gt;)/gi;
-    var plain = unescape(content.replace(re, ''));
-
-    // remove duplicated words
-    var words = plain.split(' ');
-    var deduped = [...new Set(words)];
-    var dedupedStr = deduped.join(' ');
-
-    // remove short and less meaningful words
-    var result = dedupedStr.replace(
-      /\b(\.|\,|the|a|an|and|am|you|I|to|if|of|off|me|my|on|in|it|is|at|as|we|do|be|has|but|was|so|no|not|or|up|for)\b/gi,
-      ''
-    );
-    //remove newlines, and punctuation
-    result = result.replace(/\.|\,|\?|-|—|\n/g, '');
-    //remove repeated spaces
-    result = result.replace(/[ ]{2,}/g, ' ');
-
-    return result;
-  });
-
   // Get the first `n` elements of a collection.
   eleventyConfig.addFilter('head', (array, n) => {
     if (n < 0) {
