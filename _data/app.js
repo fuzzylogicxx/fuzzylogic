b3d6d9527a6ebbf1e94a2806b2d84d943090b169
Laurence Hughes <laurencehughes1976@gmail.com>
2025-06-22 14:04:10 +0100

    2025 Housekeeping and modernisation, Part 1 (#172)
    * Deletes Sass and purgecss-based-building. Now uses CSS and 11ty’s built-in bundling
    * Renames 11ty config file to the modern convention
    * Removes unnecessary top-level 11ty-cache-assets dependency
    * Makes a variable named 'environment' available to files which can’t access process.env directly, such as Nunjucks files
    * Converts a couple of data files from JSON to JS which makes them a bit more flexible and future-friendly

---
+++ _data/app.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // Make a varianble named “environment” available to files which can’t access process.env directly (.njk files etc).
+  // If the NODE_ENV environment variable is defined (perhaps in a npm script, or in Netlify dasboard),
+  // use its value. Otherwise assume "development".
+  environment: process.env.NODE_ENV || "development"
+};
