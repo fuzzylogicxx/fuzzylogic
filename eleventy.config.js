57b6b63dafa93a57acaaf651f27ed5ad4d2745d7
Laurence Hughes <laurence@fuzzylogicdigital.com>
2026-01-02 11:52:01 +0000

    Tweaks 11ty image plugin config to output jpg too. I need that for open graph images so images display in apple iMessage

--- eleventy.config.js
+++ eleventy.config.js
@@ -156,7 +156,7 @@ export default async function(eleventyConfig) {
 	// Image optimization: https://www.11ty.dev/docs/plugins/image/#eleventy-transform
 	eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
 		// Output formats for each image.
-		formats: ["avif", "webp"],
+    formats: ["avif", "webp", "jpeg"],
 		widths: [800, "auto"],
 		// failOnError: false,
 		htmlOptions: {
